ebfae9a0f2dd0c211ca9a4c193513c9729dbef0c
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-10-24 09:17:00 -0200

    Criando Classe Loterica

---
+++ src/lotofacil/Loterica.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lotofacil;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author tiago
+ */
+public class Loterica {
+    
+    private ArrayList<Integer> numerosSorteados;
+
+    public Loterica() {
+        this.numerosSorteados = new ArrayList<>();
+    }
+    
+    
+    
+}
